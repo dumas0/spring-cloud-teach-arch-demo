7e4e2c9f91e0e44f2ed6f5ac591e7ef3e91f58e7
dumas <dz.ma@shunwang.com>
2022-05-03 10:57:48 +0800

    Nacos 注册中心 目录结构更正

--- scta-register-center/scta-register-center-nacos/scta-register-center-nacos-consumer/src/main/java/com/dumas/scta/nacos/config/consumer/NacosConfigConsumerApplication.java
+++ scta-register-center/scta-register-center-nacos/scta-register-center-nacos-consumer/src/main/java/com/dumas/scta/nacos/config/consumer/NacosConfigConsumerApplication.java
@@ -2,7 +2,6 @@ package com.dumas.scta.nacos.config.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author dumas

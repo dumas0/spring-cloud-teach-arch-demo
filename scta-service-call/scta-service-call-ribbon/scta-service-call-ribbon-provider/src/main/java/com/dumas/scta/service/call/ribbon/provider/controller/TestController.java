77e712e17420f9cbb02530da217b954049406f27
dumas <dz.ma@shunwang.com>
2022-05-04 10:46:59 +0800

    包导入

--- scta-service-call/scta-service-call-ribbon/scta-service-call-ribbon-provider/src/main/java/com/dumas/scta/service/call/ribbon/provider/controller/TestController.java
+++ scta-service-call/scta-service-call-ribbon/scta-service-call-ribbon-provider/src/main/java/com/dumas/scta/service/call/ribbon/provider/controller/TestController.java
@@ -5,6 +5,13 @@ package com.dumas.scta.service.call.ribbon.provider.controller;
  * @desc TODO
  * @date 2022/05/04 00:10
  */
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class TestController {
     private Logger logger = LoggerFactory.getLogger(TestController.class);

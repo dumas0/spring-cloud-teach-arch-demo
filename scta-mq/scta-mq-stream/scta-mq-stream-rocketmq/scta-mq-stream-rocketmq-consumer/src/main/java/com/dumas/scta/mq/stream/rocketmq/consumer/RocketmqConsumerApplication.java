7e336f3822476c7e4745cf7734f74a1f5d13c341
dumas <dz.ma@shunwang.com>
2022-05-04 11:08:53 +0800

    rocketmq stream 基础构建

---
+++ scta-mq/scta-mq-stream/scta-mq-stream-rocketmq/scta-mq-stream-rocketmq-consumer/src/main/java/com/dumas/scta/mq/stream/rocketmq/consumer/RocketmqConsumerApplication.java
@@ -0,0 +1,16 @@
+package com.dumas.scta.mq.stream.rocketmq.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dumas
+ * @desc TODO
+ * @date 2022/05/04 11:06
+ */
+@SpringBootApplication
+public class RocketmqConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqConsumerApplication.class, args);
+    }
+}
---
+++ scta-mq/scta-mq-stream/scta-mq-stream-rocketmq/scta-mq-stream-rocketmq-provider/src/main/java/com/dumas/scta/mq/stream/rocketmq/provider/RocketmqProviderApplication.java
@@ -0,0 +1,16 @@
+package com.dumas.scta.mq.stream.rocketmq.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dumas
+ * @desc TODO
+ * @date 2022/05/04 11:04
+ */
+@SpringBootApplication
+public class RocketmqProviderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqProviderApplication.class, args);
+    }
+}

ccb53a28a7b6bf82106bc9d483a2aa32ca0c918d
dumas <dz.ma@shunwang.com>
2022-05-16 14:56:53 +0800

    定时消息/延时消息

--- scta-mq/scta-mq-stream/scta-mq-stream-rocketmq/scta-mq-stream-rocketmq-producer/src/main/java/com/dumas/scta/mq/stream/rocketmq/provider/controller/Demo01Controller.java
+++ scta-mq/scta-mq-stream/scta-mq-stream-rocketmq/scta-mq-stream-rocketmq-producer/src/main/java/com/dumas/scta/mq/stream/rocketmq/provider/controller/Demo01Controller.java
@@ -2,6 +2,9 @@ package com.dumas.scta.mq.stream.rocketmq.provider.controller;
 
 import com.dumas.scta.mq.stream.rocketmq.provider.message.Demo01Message;
 import com.dumas.scta.mq.stream.rocketmq.provider.message.MySource;
+import org.apache.rocketmq.common.message.MessageConst;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
@@ -20,6 +23,8 @@ import java.util.Random;
 @RequestMapping("/demo01")
 public class Demo01Controller {
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     @Autowired
     private MySource mySource;
 
@@ -33,5 +38,24 @@ public class Demo01Controller {
                 .build();
         // <3> 发送消息
         return mySource.demo01Output().send(springMessage);
+    }
+
+    /**
+     * 定时消息/延迟消息
+     * @return
+     */
+    @GetMapping("send_delay")
+    public boolean sendDelay() {
+        // 创建 Message
+        Demo01Message message = new Demo01Message()
+                .setId(new Random().nextInt());
+        // 创建 Spring Message 对象 设置延迟级别为 3，10 秒后消费
+        Message<Demo01Message> springMessage = MessageBuilder.withPayload(message)
+                .setHeader(MessageConst.PROPERTY_DELAY_TIME_LEVEL, "3")
+                .build();
+        // 发送消息
+        boolean sendResult = mySource.demo01Output().send(springMessage);
+        logger.info("[sendDelay][发送消息完成，结果 = {}]", sendResult);
+        return sendResult;
     }
 }

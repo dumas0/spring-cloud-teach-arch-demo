9d81ddd8ea3c25fc874737b401722c44ef57a469
dumas <dz.ma@shunwang.com>
2022-05-18 08:26:57 +0800

    事务消息

---
+++ scta-mq/scta-mq-stream/scta-mq-stream-rocketmq/scta-mq-stream-rocketmq-producer-transaction/src/main/java/com/dumas/scta/mq/stream/rocketmq/transaction/RocketmqProducerTransactionApplication.java
@@ -0,0 +1,19 @@
+package com.dumas.scta.mq.stream.rocketmq.transaction;
+
+import com.dumas.scta.mq.stream.rocketmq.transaction.message.MySource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+
+/**
+ * @author dumas
+ * @desc TODO
+ * @date 2022/05/04 11:04
+ */
+@EnableBinding(MySource.class)
+@SpringBootApplication
+public class RocketmqProducerTransactionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqProducerTransactionApplication.class, args);
+    }
+}
---
+++ scta-mq/scta-mq-stream/scta-mq-stream-rocketmq/scta-mq-stream-rocketmq-producer-transaction/src/main/java/com/dumas/scta/mq/stream/rocketmq/transaction/message/MySource.java
@@ -0,0 +1,14 @@
+package com.dumas.scta.mq.stream.rocketmq.transaction.message;
+
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.messaging.MessageChannel;
+
+/**
+ * @author dumas
+ * @desc TODO
+ * @date 2022/05/04 21:47
+ */
+public interface MySource {
+    @Output("demo01-output")
+    MessageChannel demo01Output();
+}

40bbb3a1ad75cf1633869af035e34d0592de8085
dumas <dz.ma@shunwang.com>
2022-05-18 17:53:47 +0800

    广播消息

---
+++ scta-mq/scta-mq-stream/scta-mq-stream-rabbitmq/scta-mq-stream-rabbitmq-consumer-broadcast/src/test/java/com/dumas/scta/mq/stream/rabbitmq/consumer/RabbitmqConsumerApplicationTest.java
@@ -0,0 +1,11 @@
+package com.dumas.scta.mq.stream.rabbitmq.consumer;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author dumas
+ * @date 2022/05/18 3:35 PM
+ */
+@SpringBootTest
+public class RabbitmqConsumerApplicationTest {
+}

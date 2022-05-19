637f9161323eb1019c6b64fbffdd20f3e163e244
dumas <dz.ma@shunwang.com>
2022-05-19 11:22:12 +0800

    消息重试

---
+++ scta-mq/scta-mq-stream/scta-mq-stream-kafka/scta-mq-stream-kafka-consumer-retry/src/main/java/com/dumas/scta/mq/stream/kafka/retry/listener/Demo01Consumer.java
@@ -0,0 +1,24 @@
+package com.dumas.scta.mq.stream.kafka.retry.listener;
+
+import com.dumas.scta.mq.stream.kafka.retry.message.Demo01Message;
+import com.dumas.scta.mq.stream.kafka.retry.message.MySink;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author dumas
+ * @date 2022/05/19 11:11 AM
+ */
+@Component
+public class Demo01Consumer {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @StreamListener(MySink.DEMO01_INPUT)
+    public void onMessage(@Payload Demo01Message message) {
+        logger.info("[onMessage][线程编号:{} 消息内容：{}]", Thread.currentThread().getId(), message);
+        throw new RuntimeException("我就是故意抛出一个异常");
+    }
+}
---
+++ scta-mq/scta-mq-stream/scta-mq-stream-kafka/scta-mq-stream-kafka-consumer-retry/src/test/java/com/dumas/scta/mq/stream/kafka/retry/KafkaConsumserRetryApplicationTest.java
@@ -0,0 +1,12 @@
+package com.dumas.scta.mq.stream.kafka.retry;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author dumas
+ * @date 2022/05/19 10:48 AM
+ */
+@SpringBootTest
+public class KafkaConsumserRetryApplicationTest {
+
+}

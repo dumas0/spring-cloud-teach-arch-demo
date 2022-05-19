82347c3c514d8354a6c91eedc5b792b7e8592dbc
dumas <dz.ma@shunwang.com>
2022-05-19 15:31:57 +0800

    消息的提交机制

---
+++ scta-mq/scta-mq-stream/scta-mq-stream-kafka/scta-mq-stream-kafka-consumer-ack/src/main/java/com/dumas/scta/mq/stream/kafka/ack/KafkaConsumerAckApplication.java
@@ -0,0 +1,18 @@
+package com.dumas.scta.mq.stream.kafka.ack;
+
+import com.dumas.scta.mq.stream.kafka.ack.message.MySink;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+
+/**
+ * @author dumas
+ * @date 2022/05/19 10:46 AM
+ */
+@EnableBinding(MySink.class)
+@SpringBootApplication
+public class KafkaConsumerAckApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaConsumerAckApplication.class, args);
+    }
+}
---
+++ scta-mq/scta-mq-stream/scta-mq-stream-kafka/scta-mq-stream-kafka-consumer-ack/src/main/java/com/dumas/scta/mq/stream/kafka/ack/listener/Demo01Consumer.java
@@ -0,0 +1,35 @@
+package com.dumas.scta.mq.stream.kafka.ack.listener;
+
+import com.dumas.scta.mq.stream.kafka.ack.message.Demo01Message;
+import com.dumas.scta.mq.stream.kafka.ack.message.MySink;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author dumas
+ * @date 2022/05/19 11:11 AM
+ */
+@Component
+public class Demo01Consumer {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    private AtomicInteger index = new AtomicInteger();
+
+    @StreamListener(MySink.DEMO01_INPUT)
+    public void onMessage(@Payload Demo01Message message,
+                          @Header(KafkaHeaders.ACKNOWLEDGMENT) Acknowledgment acknowledgment) {
+        logger.info("[onMessage][线程编号:{} 消息内容：{}]", Thread.currentThread().getId(), message);
+        // 提交消费进度
+        if (index.incrementAndGet() == 1) {
+            acknowledgment.acknowledge();
+        }
+    }
+}

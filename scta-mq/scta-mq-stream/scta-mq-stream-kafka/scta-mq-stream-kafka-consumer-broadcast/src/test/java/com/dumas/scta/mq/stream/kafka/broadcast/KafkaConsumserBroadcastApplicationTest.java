41d6ed82c56d5228d4ec612f3f6dadc69c423f80
dumas <dz.ma@shunwang.com>
2022-05-19 11:32:00 +0800

    广播消息

---
+++ scta-mq/scta-mq-stream/scta-mq-stream-kafka/scta-mq-stream-kafka-consumer-broadcast/src/test/java/com/dumas/scta/mq/stream/kafka/broadcast/KafkaConsumserBroadcastApplicationTest.java
@@ -0,0 +1,12 @@
+package com.dumas.scta.mq.stream.kafka.broadcast;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author dumas
+ * @date 2022/05/19 10:48 AM
+ */
+@SpringBootTest
+public class KafkaConsumserBroadcastApplicationTest {
+
+}

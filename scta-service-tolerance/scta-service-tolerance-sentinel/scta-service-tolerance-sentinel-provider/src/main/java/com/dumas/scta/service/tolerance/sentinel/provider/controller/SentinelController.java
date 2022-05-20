839817899d4a78fb14e341383eb6c5c394241fd5
dumas <dz.ma@shunwang.com>
2022-05-20 17:34:27 +0800

    内部使用Sentinel Api进行资源保护

--- scta-service-tolerance/scta-service-tolerance-sentinel/scta-service-tolerance-sentinel-provider/src/main/java/com/dumas/scta/service/tolerance/sentinel/provider/controller/SentinelController.java
+++ scta-service-tolerance/scta-service-tolerance-sentinel/scta-service-tolerance-sentinel-provider/src/main/java/com/dumas/scta/service/tolerance/sentinel/provider/controller/SentinelController.java
@@ -1,6 +1,10 @@
 package com.dumas.scta.service.tolerance.sentinel.provider.controller;
 
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.Sph;
+import com.alibaba.csp.sentinel.SphU;
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,5 +37,25 @@ public class SentinelController {
     @SentinelResource("demo_product_info_hot")
     public String productInfo(Integer id) {
         return "商品编号：" + id;
+    }
+
+    @GetMapping("/entry_demo")
+    public String entryDemo() {
+        Entry entry = null;
+        try {
+            // 访问资源
+            entry = SphU.entry("entry_demo");
+
+            // 执行业务逻辑
+
+            return "执行成功";
+        } catch (BlockException ex) {
+            return "被拒绝";
+        } finally {
+            // 释放资源
+            if (entry != null) {
+                entry.exit();
+            }
+        }
     }
 }

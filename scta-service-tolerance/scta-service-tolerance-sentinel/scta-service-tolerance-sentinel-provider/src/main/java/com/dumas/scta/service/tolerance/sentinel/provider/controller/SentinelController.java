96d000b42faec5bc699876d50a4d5f8bb22e00b3
dumas <dz.ma@shunwang.com>
2022-05-20 17:50:37 +0800

    注解支持异常处理

--- scta-service-tolerance/scta-service-tolerance-sentinel/scta-service-tolerance-sentinel-provider/src/main/java/com/dumas/scta/service/tolerance/sentinel/provider/controller/SentinelController.java
+++ scta-service-tolerance/scta-service-tolerance-sentinel/scta-service-tolerance-sentinel-provider/src/main/java/com/dumas/scta/service/tolerance/sentinel/provider/controller/SentinelController.java
@@ -7,6 +7,7 @@ import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -57,5 +58,24 @@ public class SentinelController {
                 entry.exit();
             }
         }
+    }
+
+    @GetMapping("/annotations_demo")
+    @SentinelResource(value = "annotations_demo_resource", blockHandler = "blockHandler", fallback = "fallback")
+    public String annotationsDemo(@RequestParam(required = false) Integer id){
+        if(id == null){
+            throw new IllegalArgumentException("id 参数不允许为空");
+        }
+        return "success";
+    }
+
+    // BlockHandler 处理函数，参数最多一个 BlockException,其余与原函数一致.
+    public String blockHandler(Integer id, BlockException ex){
+        return "block:" + ex.getClass().getSimpleName();
+    }
+
+    // Fallback 处理函数，函数签名与原函数一致或加一个 Throwable 类型的参数.
+    public String fallback(Integer id, Throwable throwable){
+        return "fallback:" + throwable.getMessage();
     }
 }

8061094303991a1bb4440293ac623558b405c22d
dumas <dz.ma@shunwang.com>
2022-05-20 15:33:13 +0800

    sentinel 流量控制

---
+++ scta-service-tolerance/scta-service-tolerance-sentinel/scta-service-tolerance-sentinel-provider/src/main/java/com/dumas/scta/service/tolerance/sentinel/provider/config/CustomRequestOriginParser.java
@@ -0,0 +1,25 @@
+package com.dumas.scta.service.tolerance.sentinel.provider.config;
+
+import com.alibaba.cloud.commons.lang.StringUtils;
+import com.alibaba.csp.sentinel.adapter.spring.webmvc.callback.RequestOriginParser;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author dumas
+ * @date 2022/05/20 3:30 PM
+ */
+@Component
+public class CustomRequestOriginParser implements RequestOriginParser {
+    @Override
+    public String parseOrigin(HttpServletRequest httpServletRequest) {
+        // 从 header 中获得请求来源
+        String origin = httpServletRequest.getHeader("x-user");
+        // 如果为空给一个默认的值
+        if (StringUtils.isEmpty(origin)) {
+            origin = "default";
+        }
+        return origin;
+    }
+}
---
+++ scta-service-tolerance/scta-service-tolerance-sentinel/scta-service-tolerance-sentinel-provider/src/main/java/com/dumas/scta/service/tolerance/sentinel/provider/controller/SentinelController.java
@@ -0,0 +1,24 @@
+package com.dumas.scta.service.tolerance.sentinel.provider.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author dumas
+ * @date 2022/05/20 3:23 PM
+ */
+@RestController
+@RequestMapping("/demo")
+public class SentinelController {
+
+    @GetMapping("/echo")
+    public String echo() {
+        return "echo";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+}

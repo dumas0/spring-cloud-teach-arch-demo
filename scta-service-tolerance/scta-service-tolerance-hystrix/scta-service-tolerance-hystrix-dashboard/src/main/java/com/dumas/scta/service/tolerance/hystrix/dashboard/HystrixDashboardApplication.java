07d4fd05c0216607b7d47500c3bae93afb1bf941
dumas <dz.ma@shunwang.com>
2022-05-23 17:10:17 +0800

    Hystrix dashboard 面板

---
+++ scta-service-tolerance/scta-service-tolerance-hystrix/scta-service-tolerance-hystrix-dashboard/src/main/java/com/dumas/scta/service/tolerance/hystrix/dashboard/HystrixDashboardApplication.java
@@ -0,0 +1,17 @@
+package com.dumas.scta.service.tolerance.hystrix.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author dumas
+ * @date 2022/05/23 5:06 PM
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class, args);
+    }
+}

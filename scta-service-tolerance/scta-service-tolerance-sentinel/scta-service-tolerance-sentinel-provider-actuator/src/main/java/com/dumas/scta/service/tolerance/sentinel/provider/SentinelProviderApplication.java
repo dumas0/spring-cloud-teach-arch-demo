4b46d4f16b0fdb2b9c7fe8c3c186e88702e7e857
dumas <dz.ma@shunwang.com>
2022-05-23 11:38:22 +0800

    sentinel 整合 actuator 端点监控

---
+++ scta-service-tolerance/scta-service-tolerance-sentinel/scta-service-tolerance-sentinel-provider-actuator/src/main/java/com/dumas/scta/service/tolerance/sentinel/provider/SentinelProviderApplication.java
@@ -0,0 +1,15 @@
+package com.dumas.scta.service.tolerance.sentinel.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dumas
+ * @date 2022/05/20 3:20 PM
+ */
+@SpringBootApplication
+public class SentinelProviderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelProviderApplication.class, args);
+    }
+}

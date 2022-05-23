bf54e162ad22d2040fb43b2fcacd9cabd3af8399
dumas <dz.ma@shunwang.com>
2022-05-23 15:15:15 +0800

    用户服务

---
+++ scta-service-tolerance/scta-service-tolerance-hystrix/scta-service-tolerance-hystrix-user-service/src/main/java/com/dumas/scta/service/tolerance/hystrix/user/service/UserServiceApplication.java
@@ -0,0 +1,15 @@
+package com.dumas.scta.service.tolerance.hystrix.user.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dumas
+ * @date 2022/05/23 3:09 PM
+ */
+@SpringBootApplication
+public class UserServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+}
---
+++ scta-service-tolerance/scta-service-tolerance-hystrix/scta-service-tolerance-hystrix-user-service/src/test/java/com/dumas/scta/service/tolerance/hystrix/user/service/UserServiceApplicationTest.java
@@ -0,0 +1,11 @@
+package com.dumas.scta.service.tolerance.hystrix.user.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author dumas
+ * @date 2022/05/23 3:14 PM
+ */
+@SpringBootTest
+public class UserServiceApplicationTest {
+}

b7d68cb30116fbc71f2d187d5c08700dcda51c01
dumas <dz.ma@shunwang.com>
2022-05-24 11:01:04 +0800

    Hystrix集成Turbine

---
+++ scta-service-tolerance/scta-service-tolerance-hystrix/scta-service-tolerance-hystrix-dashboard-turbine/src/main/java/com/dumas/scta/service/tolerance/hystrix/dashboard/HystrixDashboardApplication.java
@@ -0,0 +1,19 @@
+package com.dumas.scta.service.tolerance.hystrix.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+/**
+ * @author dumas
+ * @date 2022/05/23 5:06 PM
+ */
+@EnableTurbine
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class, args);
+    }
+}
---
+++ scta-service-tolerance/scta-service-tolerance-hystrix/scta-service-tolerance-hystrix-dashboard-turbine/src/test/java/com/dumas/scta/service/tolerance/hystrix/dashboard/HystrixDashboardApplicationTest.java
@@ -0,0 +1,12 @@
+package com.dumas.scta.service.tolerance.hystrix.dashboard;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author dumas
+ * @date 2022/05/23 5:09 PM
+ */
+@SpringBootTest
+public class HystrixDashboardApplicationTest {
+
+}
